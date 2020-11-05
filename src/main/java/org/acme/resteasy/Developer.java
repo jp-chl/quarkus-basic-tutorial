480109b0a5c98e7374cb4f175c0fe5ac19a0dc65
jp-chl <vera.r.juan.pablo@gmail.com>
2020-11-05 14:41:41 -0300

    New column added to Developer entity

--- src/main/java/org/acme/resteasy/Developer.java
+++ src/main/java/org/acme/resteasy/Developer.java
@@ -1,5 +1,6 @@
 package org.acme.resteasy;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
@@ -7,9 +8,12 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 @Entity
 public class Developer extends PanacheEntity {
     
-    // It will run as a private (optional)
+    // It could run as a private (optional)
     //
     // It will be private at building time,
     // and setters/getters are automatic
     public String name;
+
+    @Column(columnDefinition = "varchar(255) default 'Quarkus'")
+    public String favouriteFramework;
 }
